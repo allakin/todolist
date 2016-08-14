3e6edb4fc47526e6348d34e6cc1fb20eaa80b6b0
Pavel <19paul91@mail.ru>
2016-08-14 15:58:28 +0300

    add code todolist

--- Tabbed Aplication/Tabbed Aplication/FirstViewController.swift
+++ Tabbed Aplication/Tabbed Aplication/FirstViewController.swift
@@ -8,10 +8,13 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+	@IBOutlet weak var tb1Tasks: UITableView!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		tb1Tasks.reloadData()
 		// Do any additional setup after loading the view, typically from a nib.
 	}
 
@@ -19,7 +22,30 @@ class FirstViewController: UIViewController {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
+	
+	override func viewWillAppear(animated: Bool) {
+		super.viewWillAppear(animated)
+		tb1Tasks.reloadData()
+	}
+	
+	func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return taskMgr.tasks.count
+	}
+	
+	func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+		let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "Default Tasks")
+		cell.textLabel?.text = taskMgr.tasks[indexPath.row].name
+		cell.detailTextLabel?.text = taskMgr.tasks[indexPath.row].desc
+		return cell
+	}
 
+	//удаление ячейки с перезагрузкой tb1Tasks
+	func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+		if (editingStyle == UITableViewCellEditingStyle.Delete){
+			taskMgr.tasks.removeAtIndex(indexPath.row)
+			tb1Tasks.reloadData()
+		}
+	}
 
 }
 
--- Tabbed Aplication/Tabbed Aplication/SecondViewController.swift
+++ Tabbed Aplication/Tabbed Aplication/SecondViewController.swift
@@ -8,8 +8,29 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate {
 
+	@IBOutlet weak var txtTask: UITextField!
+	@IBOutlet weak var txtDesc: UITextField!
+	
+	@IBAction func btnAddTask(sender: UIButton){
+		// запись
+		taskMgr.addTask(txtTask.text!, desc: txtDesc.text!)
+		self.view.endEditing(true)
+		txtTask.text = ""
+		txtDesc.text = ""
+		self.tabBarController?.selectedIndex = 0
+	}
+	
+//	override func touchesBegan(touches: NSSet!, withEvent event: UIEvent!) {
+//		self.view.endEditing(true)
+//	}
+	
+	func textFieldShouldReturn(textField: UITextField) -> Bool {
+		textField.resignFirstResponder()
+		return true
+	}
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
